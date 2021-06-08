f4e3aefdffc5e500b7fc0ab700f1ce98db56c5b4
NinaAurora <ninika@gmail.com>
2021-06-08 16:13:41 +0200

    Second commit

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 
 <title> My Blog</title>
-<link rel="stylesheet" href="app.css">
+<link rel="stylesheet" href="/app.css">
+<link rel="stylesheet" href="/vendor-lib.css">
 
 <body>
 
     <article>
 
+        <h1> {{$post->title}} </h1>
 
+        <div>
+            {!! $post->body !!}
+        </div>
 
     </article>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 
 <title> My Blog</title>
-<link rel="stylesheet" href="app.css">
+<link rel="stylesheet" href="/app.css">
+<link rel="stylesheet" href="/vendor-lib.css">
 
 <body>
 
-    <?php foreach ($posts as $post) : ?>
-    <article>
-        <?= $post; ?>
+    @foreach ($posts as $post)
+    <article class="{{ $loop->even ? 'mb-60' : ''}}">
+
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
+        </h1>
+
+        <div>
+            {{$post->excerpt}}
+        </div>
+
     </article>
-    <?php endforeach; ?>
+    @endforeach
 
 </body>
